f4788684806da272765c705a604cc0a64453e3d8
Prajwal-PK <prajwalkale23pk@gmail.com>
2024-08-14 20:02:48 +0530

    Update recommender_app.py

--- Codes/recommender_app.py
+++ Codes/recommender_app.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestRegressor
+import os
+
+# Print the current working directory
+st.write("Current Working Directory:", os.getcwd())
 
 # Load data and model (ensure correct file paths)
 data = pickle.load(open('data.pkl', 'rb'))
